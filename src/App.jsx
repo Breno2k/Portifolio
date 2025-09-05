8f387ce267617580f306eea68d1cd88c08e39575
Breno Melo <brenolucasmelo@gmail.com>
2025-09-05 15:27:15 -0300

    feat: react-router adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+
+// Components
 import Navbar from './components/navbar/Navbar'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
+
+// Hooks
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
