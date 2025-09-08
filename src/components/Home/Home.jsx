08e629484fb439b428ebc68d15ad46ec8b615a6c
Breno Melo <brenolucasmelo@gmail.com>
2025-09-08 15:32:01 -0300

    feat: implementação da home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,41 @@
 import styles from './Home.module.css'
+import { FaGithub } from "react-icons/fa6";
+import { BsInstagram, BsLinkedin } from "react-icons/bs";
+import img1 from '../../assets/astronauta.png'
 
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className={styles.home}>
+            <div className={styles.container}>
+                <h1>Desenvolvedor <span>Front-end</span></h1>
+                <p> Em busca de oportunidades para criar soluções
+                    inovadoras, afim de otimizar processos e
+                    gerar impacto positivo em negócios.
+                </p>
+                <div className={styles.program}>
+                    <p>React</p>
+                    <p>JavaScript</p>
+                    <p>Next.js</p>
+                    <p>TypeScript</p>
+                </div>
+                <div className={styles.buttons_social}>
+                    <a href="https://github.com/Breno2k">
+                        <FaGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/breno-melo-69602b267/">
+                        <BsLinkedin />
+                    </a>
+                    <a href="https://www.instagram.com/brenomelo9/">
+                        <BsInstagram />
+                    </a>
+                </div>
+            </div>
+            <div className={styles.img_home}>
+                <img src={img1} alt="foto home" />
+            </div>
+        </div>
+
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from './Navbar.module.css'
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
